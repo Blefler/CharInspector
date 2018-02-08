e5c054f9395ef51c341e416105c577dfd349223a
Blefler <blefler@mail.csuchico.edu>
2018-02-07 18:05:08 -0800

    Fixed errors.

--- main.cpp
+++ main.cpp
@@ -9,20 +9,20 @@ char letter;
   cout<<"What character do you want to know about?\n";
   cin>>letter;
   //when user's entry is between A-Z...
-  if (letter<=90 || letter>=65)
+  if (letter<91&&letter>64)
   {
-  cout<<" is an upper case letter!\n";
+  cout<<letter<<  " is an upper case letter!\n";
   }
   //when user's entry is between a-z...
-  else if (letter>90)
+  else if (letter>96&&letter<123)
   {
-  cout<<" is a lower case letter!\n";
+  cout<<letter<<  " is a lower case letter!\n";
   }
 
   //in all other cases...
   else
   {
-  cout<<"?! Pssh. What are you talking about?\n";
+  cout<<letter<< "?! Pssh. What are you talking about?\n";
   }
   //no matter what they enter...
   cout<<"The ASCII value is: " << (int) letter << endl;

7419b7c0436dd0eb7af0f3605e9b85872fcab798
Blefler <blefler@mail.csuchico.edu>
2018-02-07 17:48:35 -0800

    Created a working program

--- main.cpp
+++ main.cpp
@@ -5,20 +5,26 @@ using namespace std;
 
 int main()
 {
-
+char letter;
   cout<<"What character do you want to know about?\n";
-
+  cin>>letter;
   //when user's entry is between A-Z...
+  if (letter<=90 || letter>=65)
+  {
   cout<<" is an upper case letter!\n";
-
+  }
   //when user's entry is between a-z...
+  else if (letter>90)
+  {
   cout<<" is a lower case letter!\n";
+  }
 
   //in all other cases...
+  else
+  {
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
-
+  cout<<"The ASCII value is: " << (int) letter << endl;
   return 0;
 }
